e2a3d5edb99e5534b16fedceec24e49a79b8f391
gongxini <hyigong1@jhu.edu>
2023-07-27 15:18:52 -0600

    update lib

--- setup_pbox_lib.sh
+++ setup_pbox_lib.sh
@@ -3,3 +3,8 @@
 mkdir -p build && cd build
 cmake ..
 make -j $(nproc)
+cd ..
+cp `pwd`/build/libs/libpsandbox.so `pwd`/../script/libpsandbox_psandbox.so
+echo "export PSANDBOXDIR=`pwd`" >> $HOME/.bashrc
+echo "export LD_LIBRARY_PATH=`pwd`/build/libs:$LD_LIBRARY_PATH" >> $HOME/.bashrc
+source ~/.bashrc
